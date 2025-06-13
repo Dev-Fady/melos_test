d361c02564b254b080c53b6d93a93e3ae6a55305
fady emil <fadymobiledev@gmail.com>
2025-06-13 22:43:22 +0300

    Add Failure class for error handling

---
+++ core/services/lib/error_handler/failure.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+// import 'package:domain/model/localised_message.dart';
+
+class Failure {
+  int code;
+  String message;
+  Failure({
+    required this.code,
+    required this.message,
+  });
+}

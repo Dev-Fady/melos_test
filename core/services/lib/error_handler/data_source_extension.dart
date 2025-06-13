46d38345c023a927b26212860c997e2e5a51f429
fady emil <fadymobiledev@gmail.com>
2025-06-13 22:43:11 +0300

    Add DataSourceExtension for enhanced error handling

---
+++ core/services/lib/error_handler/data_source_extension.dart
@@ -0,0 +1,69 @@
+import 'package:services/error_handler/data_source.dart';
+import 'package:services/error_handler/failure.dart';
+import 'package:services/error_handler/response_code.dart';
+import 'package:services/error_handler/response_message.dart';
+
+extension DataSourceExtension on DataSource {
+  Failure getFailure() {
+    switch (this) {
+      case DataSource.success:
+        return Failure(
+            code: ResponseCode.success, message: ResponseMessage.success);
+
+      case DataSource.noContent:
+        return Failure(
+            code: ResponseCode.noContent, message: ResponseMessage.noContent);
+
+      case DataSource.badRequest:
+        return Failure(
+            code: ResponseCode.badRequest, message: ResponseMessage.badRequest);
+
+      case DataSource.forbidden:
+        return Failure(
+            code: ResponseCode.forbidden, message: ResponseMessage.forbidden);
+
+      case DataSource.unauthorized:
+        return Failure(
+            code: ResponseCode.unauthorized,
+            message: ResponseMessage.unauthorized);
+
+      case DataSource.internalServerError:
+        return Failure(
+            code: ResponseCode.internalServerError,
+            message: ResponseMessage.internalServerError);
+
+      case DataSource.connectionTimeout:
+        return Failure(
+            code: ResponseCode.connectTimeout,
+            message: ResponseMessage.connectTimeout);
+
+      case DataSource.cancelled:
+        return Failure(
+            code: ResponseCode.cancelled, message: ResponseMessage.cancelled);
+
+      case DataSource.receiveTimeout:
+        return Failure(
+            code: ResponseCode.receiveTimeout,
+            message: ResponseMessage.receiveTimeout);
+
+      case DataSource.sendTimeout:
+        return Failure(
+            code: ResponseCode.sendTimeout,
+            message: ResponseMessage.sendTimeout);
+
+      case DataSource.cacheError:
+        return Failure(
+            code: ResponseCode.cacheError, message: ResponseMessage.cacheError);
+
+      case DataSource.notInternetConnection:
+        return Failure(
+            code: ResponseCode.noInternetConnection,
+            message: ResponseMessage.noInternetConnection);
+
+      case DataSource.defaultError:
+        return Failure(
+            code: ResponseCode.defaultError,
+            message: ResponseMessage.defaultError);
+    }
+  }
+}

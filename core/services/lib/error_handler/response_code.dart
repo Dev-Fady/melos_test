2a8388f3046a53934bd68176462494e935a01d9d
fady emil <fadymobiledev@gmail.com>
2025-06-13 22:45:49 +0300

    Add ResponseCode class to define standard HTTP and local error codes

---
+++ core/services/lib/error_handler/response_code.dart
@@ -0,0 +1,18 @@
+class ResponseCode {
+  static int success = 200;
+  static int noContent = 204;
+  static int badRequest = 400;
+  static int unauthorized = 401;
+  static int forbidden = 403;
+  static int internalServerError = 500;
+  static int notFound = 404;
+
+  // local status codes
+  static int connectTimeout = -1;
+  static int cancelled = -2;
+  static int receiveTimeout = -3;
+  static int sendTimeout = -4;
+  static int cacheError = -5;
+  static int noInternetConnection = -6;
+  static int defaultError = -7;
+}

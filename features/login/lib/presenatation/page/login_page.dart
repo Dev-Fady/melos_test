5110f7b1a5cef2247a8f33a1fcc2920b04df0bf1
fady emil <fadymobiledev@gmail.com>
2025-06-14 12:22:30 +0300

    Add LoginPageBody widget to encapsulate login form functionality

--- features/login/lib/presenatation/page/login_page.dart
+++ features/login/lib/presenatation/page/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:login/presenatation/page/login_page_body.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,7 +8,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      body: LoginPageBody() ,
     );
   }
 }
---
+++ features/login/lib/presenatation/page/login_page_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:login/presenatation/widget/login_form.dart';
+
+class LoginPageBody extends StatelessWidget {
+  const LoginPageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginForm();
+  }
+}

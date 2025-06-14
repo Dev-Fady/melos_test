9beebe79949608eadeb2dc8c3e01c903c6592774
fady emil <fadymobiledev@gmail.com>
2025-06-14 15:41:57 +0300

    Fix import statements and update error logger type in LoginService

--- features/login/lib/data/service/login_service.dart
+++ features/login/lib/data/service/login_service.dart
@@ -1,8 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:login/data/model/login_model.dart';
-import 'package:retrofit/dio.dart';
+import 'package:retrofit/retrofit.dart';
 import 'package:retrofit/error_logger.dart';
-import 'package:retrofit/http.dart';
 
 part 'login_service.g.dart';
 
@@ -11,7 +10,7 @@ const String baseUrl = "https://minafarid.mocklab.io";
 @RestApi(baseUrl: baseUrl)
 abstract class LoginService {
   factory LoginService(Dio dio,
-      {String baseUrl, ParseErrorLogger errorLogger}) = _LoginService;
+      {String baseUrl, ParseErrorLogger? errorLogger}) = _LoginService;
 
   @POST("/customers/login")
   Future<HttpResponse<LoginModel>> login(

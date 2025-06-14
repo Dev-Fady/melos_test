dcf0dd92ba933d87af67f59c4f9624baf447fa01
fady emil <fadymobiledev@gmail.com>
2025-06-14 19:12:56 +0300

    Refactor dependency injection to include LoginRemoteDataSource and update NetworkInfo registration

--- lib/DI/dependency_injection.dart
+++ lib/DI/dependency_injection.dart
@@ -1,7 +1,12 @@
 import 'package:constants/app_constants.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:login/data/remote/login_remote_data_source.dart';
+import 'package:login/data/remote/login_remote_data_source_impl.dart';
+import 'package:login/data/repo/login_repo_impl.dart';
 import 'package:login/data/service/login_service.dart';
+import 'package:login/domain/repository/login_repository.dart';
 import 'package:services/factory/dio_factory.dart';
 import 'package:services/network_info/network_info.dart';
 import 'package:services/network_info/network_info_impl.dart';
@@ -9,8 +14,11 @@ import 'package:services/network_info/network_info_impl.dart';
 final getIt = GetIt.instance;
 
 Future<void> setupGetIt() async {
-  // Network Info
-  getIt.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl());
+  getIt.registerLazySingleton<InternetConnectionChecker>(
+      () => InternetConnectionChecker());
+
+  getIt.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(
+      getIt())); // getIt() here means: give me the InternetConnectionChecker
 
   // Dio Factory
   getIt.registerLazySingleton<DioFactory>(() => DioFactory(
@@ -26,4 +34,14 @@ Future<void> setupGetIt() async {
 
   // Login Service (Retrofit)
   getIt.registerLazySingleton<LoginService>(() => LoginService(dio));
+
+  // Login Remote Data Source
+  getIt.registerLazySingleton<LoginRemoteDataSource>(
+      () => LoginRemoteDataSourceImpl(
+            loginService: getIt<LoginService>(),
+            networkInfo: getIt<NetworkInfo>(),
+          ));
+
+  getIt.registerLazySingleton<LoginRepository>(() => LoginRepoImpl(
+      loginRemoteDataSourcel: getIt<LoginRemoteDataSource>()));
 }

0eba41642a34b9ec51610797f9258e83bac073e5
fady emil <fadymobiledev@gmail.com>
2025-06-14 19:05:37 +0300

    Refactor NetworkInfoImpl to use InternetConnectionChecker for connectivity checks

--- core/services/lib/network_info/network_info_impl.dart
+++ core/services/lib/network_info/network_info_impl.dart
@@ -1,13 +1,11 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:services/network_info/network_info.dart';
 
 class NetworkInfoImpl implements NetworkInfo {
+  final InternetConnectionChecker checker;
+
+  NetworkInfoImpl(this.checker);
+
   @override
-  Future<bool> get isConnected async {
-    final connectivityResult = await Connectivity().checkConnectivity();
-    final isConnectedToNetwork =
-        connectivityResult == ConnectivityResult.mobile ||
-            connectivityResult == ConnectivityResult.wifi;
-    return isConnectedToNetwork;
-  }
+  Future<bool> get isConnected => checker.hasConnection;
 }

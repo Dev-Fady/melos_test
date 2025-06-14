67ed5e6b24f3877743eaf65ddf0fa3bc1a3be68a
fady emil <fadymobiledev@gmail.com>
2025-06-14 11:22:15 +0300

    Add AppConstants class to define application-wide constants

---
+++ core/constants/lib/app_constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const String baseUrl = "https://minafarid.mocklab.io";
+
+  static const String defaultAccessToken = "Bearer your_token";
+
+  static const String defaultLanguage = "en";
+
+  static const Duration timeoutDuration = Duration(seconds: 60);
+}

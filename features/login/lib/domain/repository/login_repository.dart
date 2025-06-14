3918cafdf6e5a7b5e0dc2ec597e302fa610d1969
fady emil <fadymobiledev@gmail.com>
2025-06-14 19:10:40 +0300

    Refactor login method in LoginRepository to accept email and password parameters instead of LoginRequest

--- features/login/lib/domain/repository/login_repository.dart
+++ features/login/lib/domain/repository/login_repository.dart
@@ -1,10 +1,10 @@
 import 'package:dartz/dartz.dart';
-import 'package:login/data/request/login_request.dart';
 import 'package:login/domain/entites/login_entity.dart';
 import 'package:services/error_handler/failure.dart';
 
 abstract class LoginRepository {
   Future<Either<Failure, LoginEntity>> login({
-    required LoginRequest loginRequest,
+    required String email,
+    required String password,
   });
 }

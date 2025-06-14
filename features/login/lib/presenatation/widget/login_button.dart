a857e8d3e7a55039a16f42770f742f1731edb6cf
fady emil <fadymobiledev@gmail.com>
2025-06-14 12:12:11 +0300

    Add LoginButton widget for user login with loading state

---
+++ features/login/lib/presenatation/widget/login_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final bool isLoading;
+
+  const LoginButton({
+    super.key,
+    required this.onPressed,
+    this.isLoading = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: isLoading ? null : onPressed,
+        child: isLoading
+            ? const CircularProgressIndicator(color: Colors.white)
+            : const Text('Login'),
+      ),
+    );
+  }
+}

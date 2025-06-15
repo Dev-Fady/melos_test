61a5e4cddc2b28947645ff3d4e0c28e08d704abb
fady emil <fadymobiledev@gmail.com>
2025-06-15 17:10:05 +0300

    Refactor CardDetails to use BlocBuilder for state management and display user data dynamically

--- features/home/lib/presentation/widget/card_details.dart
+++ features/home/lib/presentation/widget/card_details.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home/presentation/cubit/home_cubit/home_cubit_cubit.dart';
 
 class CardDetails extends StatelessWidget {
   const CardDetails({
@@ -8,32 +12,52 @@ class CardDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(16),
-      elevation: 6,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage('https://api.lorem.space/image/face?w=640&h=480&r=867'),
+    return BlocBuilder<HomeCubitCubit, HomeCubitState>(
+      builder: (context, state) {
+        if (state is HomeSuccess) {
+          return Card(
+            margin: const EdgeInsets.all(16),
+            elevation: 6,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(state.homeEntity.imageAvatar),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    state.homeEntity.name,
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  Text("Email: ${state.homeEntity.email}"),
+                  Text("Password: ${state.homeEntity.password}"),
+                  Text("Role: ${state.homeEntity.role}"),
+                ],
+              ),
             ),
-            const SizedBox(height: 16),
-            Text(
-             ' userData["name"]',
-              style:
-                  const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            Text("Email: ${["email"]}"),
-            Text("Password: ${["password"]}"),
-            Text("Role: ${["role"]}"),
-          ],
-        ),
-      ),
+          );
+        } else if (state is HomeLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is HomeFailure) {
+          log(state.errorMessage);
+          return const Center(
+            child: Text('Something went wrong. Please try again.***********'),
+          );
+        } else {
+          return const Center(
+            child: Text('Something went wrong. Please try again.'),
+          );
+        }
+      },
     );
   }
 }

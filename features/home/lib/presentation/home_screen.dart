88787f7d842550e39e2f23477df345efddf278d9
fady emil <fadymobiledev@gmail.com>
2025-06-15 11:12:03 +0300

    Add HomeScreen widget to presentation layer

---
+++ features/home/lib/presentation/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:home/presentation/page/home_page.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}

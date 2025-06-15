53e92ec37c3a50ee650630832edb3c97cb5f3f0a
fady emil <fadymobiledev@gmail.com>
2025-06-15 15:28:37 +0300

    Add Home route to router configuration

--- core/routing/lib/routing.dart
+++ core/routing/lib/routing.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:home/presentation/home_screen.dart';
 import 'package:login/domain/repository/login_repository.dart';
 import 'package:login/presenatation/cubit/login/login_cubit.dart';
 import 'package:login/presenatation/login_screen.dart';
@@ -25,6 +26,13 @@ GoRouter createRouter(String initialLocation) {
                 LoginCubit(loginRepository: getIt<LoginRepository>()),
             child: LoginScreen(),
           );
+        },
+      ),
+      GoRoute(
+        path: RouterName.home,
+        name: RouterName.home,
+        builder: (context, state) {
+          return HomeScreen();
         },
       ),
     ],

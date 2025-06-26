f44ad25626abc3ead41abeda339be176d301dfe8
fady emil <fadymobiledev@gmail.com>
2025-06-26 19:31:17 +0300

    Refactor login data source to use SecureStorage for caching

--- features/login/lib/data/local_data/login_local_data_scurce_impl.dart
+++ features/login/lib/data/local_data/login_local_data_scurce_impl.dart
@@ -1,26 +1,50 @@
 import 'dart:convert';
-
-import 'package:cache/cache_helper.dart';
+// import 'package:cache/cache_helper.dart';
+import 'package:cache/secure_storage.dart';
 import 'package:constants/app_constants.dart';
 import 'package:login/data/local_data/login_local_data_scurce.dart';
 import 'package:login/data/model/login_model.dart';
 
 class LoginLocalDataScurceImpl implements LoginLocalDataSource {
-  CacheHelper prefs = CacheHelper();
+  // CacheHelper prefs = CacheHelper();
+  // @override
+  // Future<void> cacheLoginData({required LoginModel loginModel}) async {
+  //   final jsonString = jsonEncode(loginModel.toJson());
+  //   await prefs.saveData(key: AppConstants.cachedLoginKey, value: jsonString);
+  // }
+
+  // @override
+  // Future<void> clearCachedLoginData() async {
+  //   await prefs.clearData();
+  // }
+
+  // @override
+  // Future<LoginModel?> getCachedLoginData() async {
+  //   final jsonString = prefs.getData(key: AppConstants.cachedLoginKey);
+  //   if (jsonString != null) {
+  //     return LoginModel.fromJson(jsonDecode(jsonString));
+  //   }
+  //   return null;
+  // }
+
   @override
   Future<void> cacheLoginData({required LoginModel loginModel}) async {
     final jsonString = jsonEncode(loginModel.toJson());
-    await prefs.saveData(key: AppConstants.cachedLoginKey, value: jsonString);
+    // await prefs.saveData(key: AppConstants.cachedLoginKey, value: jsonString);
+    await SecureStorage.write(
+        key: AppConstants.cachedLoginKey, value: jsonString);
   }
 
   @override
   Future<void> clearCachedLoginData() async {
-    await prefs.clearData();
+    // await prefs.clearData();
+    await SecureStorage.deleteAll();
   }
 
   @override
   Future<LoginModel?> getCachedLoginData() async {
-    final jsonString = prefs.getData(key: AppConstants.cachedLoginKey);
+    final jsonString =
+        await SecureStorage.read(key: AppConstants.cachedLoginKey);
     if (jsonString != null) {
       return LoginModel.fromJson(jsonDecode(jsonString));
     }

f3ae80522defa09cc1dde4cf09b01a677e531176
fady emil <fadymobiledev@gmail.com>
2025-06-30 17:10:49 +0300

    Fix routing constant name to use bottomNavPage instead of bottonNavWithAnimatedIcons

--- core/routing/lib/router_name.dart
+++ core/routing/lib/router_name.dart
@@ -1,5 +1,5 @@
 class RouterName {
   static const String login = '/login';
   static const String home = '/home';
-  static const String bottonNavWithAnimatedIcons = '/BottonNavWithAnimatedIcons';
+  static const String bottomNavPage = '/BottomNavPage';
 }

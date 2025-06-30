0f674352f651e27896b81a5004bf2427a19da7e6
fady emil <fadymobiledev@gmail.com>
2025-06-30 17:10:33 +0300

    Implement MainNavigation widget to serve as the entry point for bottom navigation

--- features/main_navigation/lib/main_navigation.dart
+++ features/main_navigation/lib/main_navigation.dart
@@ -1,5 +1,11 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:main_navigation/presentation/page/bottom_nav_page.dart';
+
+class MainNavigation extends StatelessWidget {
+  const MainNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavPage();
+  }
 }

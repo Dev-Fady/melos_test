d5799ea3d92e8116cffa01027950325dfb526f72
fady emil <fadymobiledev@gmail.com>
2025-07-01 12:04:09 +0300

    Implement BlocProvider in HomeScreen to manage state and fetch home data

--- features/home/lib/presentation/home_screen.dart
+++ features/home/lib/presentation/home_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home/domain/repo/home_repo.dart';
+import 'package:home/presentation/cubit/home_cubit/home_cubit_cubit.dart';
 import 'package:home/presentation/page/home_page.dart';
+import 'package:melos_test/DI/dependency_injection.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return BlocProvider(
+       create: (context) => HomeCubitCubit(homeRepo:  getIt<HomeRepo>()..getHomeData() ),
+      child: HomePage(),
+    );
   }
 }

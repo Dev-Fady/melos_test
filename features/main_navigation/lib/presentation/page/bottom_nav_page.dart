9f8113a114d4862a5a44f5bac34ffd2c37c671cb
fady emil <fadymobiledev@gmail.com>
2025-07-01 12:03:45 +0300

    Refactor BottomNavPage to use widget list for navigation and improve UI display

--- features/main_navigation/lib/presentation/page/bottom_nav_page.dart
+++ features/main_navigation/lib/presentation/page/bottom_nav_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home/presentation/home_screen.dart';
 import 'package:main_navigation/presentation/widget/bottom_nav_bar.dart';
 
 class BottomNavPage extends StatefulWidget {
@@ -11,24 +12,19 @@ class BottomNavPage extends StatefulWidget {
 class _BottomNavPageState extends State<BottomNavPage> {
   int selectedNavIndex = 0;
 
-  final List<String> pages = [
-    'Home',
-    'Search',
-    'Notifications',
-    'Settings',
-    'Star',
-    'User',
+  final List<Widget> pages = [
+    Center(child: Text('Search')),
+    Center(child: Text('Notifications')),
+    Center(child: Text('Settings')),
+    Center(child: Text('Star')),
+    Center(child: Text('Star2')),
+    HomeScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Selected Page: ${pages[selectedNavIndex]}',
-          style: const TextStyle(fontSize: 24, color: Colors.black),
-        ),
-      ),
+      body: pages[selectedNavIndex],
       bottomNavigationBar: BottomNavBar(
         selectedIndex: selectedNavIndex,
         onItemSelected: (index) {

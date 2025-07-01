c9b550af0ff17199f2406fa39c28934b1db55ab4
fady emil <fadymobiledev@gmail.com>
2025-07-01 12:03:57 +0300

    Convert HomePage to StatefulWidget and implement data fetching in initState

--- features/home/lib/presentation/page/home_page.dart
+++ features/home/lib/presentation/page/home_page.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home/presentation/cubit/home_cubit/home_cubit_cubit.dart';
 import 'package:home/presentation/page/home_page_body.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<HomeCubitCubit>().getHomedata();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {

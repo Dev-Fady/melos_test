8940eb9bf420b257054abca63cf8dcaa78cb8078
Dev-Fady <fadymobiledev@gmail.com>
2025-07-10 11:57:16 +0300

    Add AllTeamsRepo abstract class for fetching team data

---
+++ features/all_teams/lib/domian/repo/all_teams_repo.dart
@@ -0,0 +1,9 @@
+import 'package:all_teams/domian/entites/all_teams_response_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:services/error_handler/failure.dart';
+
+abstract class AllTeamsRepo {
+  Future<Either<Failure, List<AllTeamsResponseEntity>>> getAllTeams({
+    required int page,
+  });
+}

8f63f6bd65e8eebcbb6e66aeda7eda42fa5c611b
Dev-Fady <fadymobiledev@gmail.com>
2025-07-10 12:24:18 +0300

    Add AllTeamModel class and generated serialization code

---
+++ features/all_teams/lib/data/model/all_team_model.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'all_team_model.g.dart';
+
+@JsonSerializable()
+class AllTeamModel {
+  String? name;
+  @JsonKey(name: "image_path")
+  String? imagePath;
+  int? founded;
+  @JsonKey(name: "last_played")
+  String? lastPlayed;
+
+  AllTeamModel({this.name, this.imagePath, this.founded, this.lastPlayed});
+
+  factory AllTeamModel.fromJson(Map<String, dynamic> json) =>
+      _$AllTeamModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AllTeamModelToJson(this);
+}
---
+++ features/all_teams/lib/data/model/all_team_model.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'all_team_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AllTeamModel _$AllTeamModelFromJson(Map<String, dynamic> json) => AllTeamModel(
+  name: json['name'] as String?,
+  imagePath: json['image_path'] as String?,
+  founded: (json['founded'] as num?)?.toInt(),
+  lastPlayed: json['last_played'] as String?,
+);
+
+Map<String, dynamic> _$AllTeamModelToJson(AllTeamModel instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'image_path': instance.imagePath,
+      'founded': instance.founded,
+      'last_played': instance.lastPlayed,
+    };

e1ea4ddaa2f4512bbda58510d9e6b868281c743e
Dev-Fady <fadymobiledev@gmail.com>
2025-07-10 11:57:05 +0300

    Add AllTeamsEntity class to represent team data structure

---
+++ features/all_teams/lib/domian/entites/all_teams_entity.dart
@@ -0,0 +1,13 @@
+class AllTeamsEntity {
+  final String name;
+  final String imagePath;
+  final int founded;
+  final int lastPlayed;
+
+  AllTeamsEntity({
+    required this.name,
+    required this.imagePath,
+    required this.founded,
+    required this.lastPlayed,
+  });
+}

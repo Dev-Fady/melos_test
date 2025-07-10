f1660a62b0910cbf532bd9e677cd3ee92d29942e
Dev-Fady <fadymobiledev@gmail.com>
2025-07-10 17:02:49 +0300

    Refactor AllTeamsCubit to improve state management and loading logic in getAllTeams method

--- features/all_teams/lib/presentation/cubit/all_teams/all_teams_cubit.dart
+++ features/all_teams/lib/presentation/cubit/all_teams/all_teams_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:all_teams/domian/entites/all_teams_response_entity.dart';
+import 'package:all_teams/domian/entites/all_teams_entity.dart';
 import 'package:all_teams/domian/repo/all_teams_repo.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
@@ -7,18 +7,42 @@ part 'all_teams_state.dart';
 
 class AllTeamsCubit extends Cubit<AllTeamsState> {
   AllTeamsCubit({required this.allTeamsRepo}) : super(AllTeamsInitial());
+
   final AllTeamsRepo allTeamsRepo;
 
-  Future<void> getAllTeams({required int page}) async {
-    emit(AllTeamsLoading());
+  List<AllTeamsEntity> teams = [];
+
+  int currentPage = 1;
+  bool hasMore = true;
+  bool isLoading = false;
+
+  Future<void> getAllTeams({int page = 1}) async {
+    if (isLoading || !hasMore) return;
+
+    isLoading = true;
+
+    if (page == 1) {
+      emit(AllTeamsLoading());
+    } else {
+      emit(AllTeamsPaginationLoading());
+    }
+
     final result = await allTeamsRepo.getAllTeams(page: page);
-    result.fold(
-      (failure) {
+
+    result.fold((failure) {
+      if (page == 1) {
         emit(AllTeamsFailure(errorMessage: failure.message));
-      },
-      (allTeamsResponseEntity) {
-        emit(AllTeamsSuccess(allTeamsResponseEntity: allTeamsResponseEntity));
-      },
-    );
+      } else {
+        emit(AllTeamsPaginationFailure(errorMessage: failure.message));
+      }
+    }, (data) {
+      teams.addAll(data.teams);
+      hasMore = data.hasMore;
+      currentPage = data.currentPage + 1;
+
+      emit(AllTeamsSuccess(teams: teams));
+    });
+
+    isLoading = false;
   }
 }

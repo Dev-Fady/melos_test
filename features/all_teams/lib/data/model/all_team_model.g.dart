809d63d3e39b7667a3ad9f2e9e632e934e4bd1db
Dev-Fady <fadymobiledev@gmail.com>
2025-07-10 17:35:41 +0300

    Refactor JSON serialization in AllTeamModel and AllTeamsResponseModel for consistency and clarity

--- features/all_teams/lib/data/model/all_team_model.g.dart
+++ features/all_teams/lib/data/model/all_team_model.g.dart
@@ -7,16 +7,18 @@ part of 'all_team_model.dart';
 // **************************************************************************
 
 AllTeamModel _$AllTeamModelFromJson(Map<String, dynamic> json) => AllTeamModel(
-  name: json['name'] as String?,
-  imagePath: json['image_path'] as String?,
-  founded: (json['founded'] as num?)?.toInt(),
-  lastPlayed: json['last_played'] as String?,
-);
+      name: json['name'] as String?,
+      imagePath: json['image_path'] as String?,
+      founded: (json['founded'] as num?)?.toInt(),
+      lastPlayed: json['last_played_at'] == null
+          ? null
+          : DateTime.parse(json['last_played_at'] as String),
+    );
 
 Map<String, dynamic> _$AllTeamModelToJson(AllTeamModel instance) =>
     <String, dynamic>{
       'name': instance.name,
       'image_path': instance.imagePath,
       'founded': instance.founded,
-      'last_played': instance.lastPlayed,
+      'last_played_at': instance.lastPlayed?.toIso8601String(),
     };
--- features/all_teams/lib/data/model/all_teams_response_model.g.dart
+++ features/all_teams/lib/data/model/all_teams_response_model.g.dart
@@ -7,28 +7,29 @@ part of 'all_teams_response_model.dart';
 // **************************************************************************
 
 AllTeamsResponseModel _$AllTeamsResponseModelFromJson(
-  Map<String, dynamic> json,
-) => AllTeamsResponseModel(
-  data: (json['data'] as List<dynamic>)
-      .map((e) => AllTeamModel.fromJson(e as Map<String, dynamic>))
-      .toList(),
-  pagination: Pagination.fromJson(json['pagination'] as Map<String, dynamic>),
-);
+        Map<String, dynamic> json) =>
+    AllTeamsResponseModel(
+      data: (json['data'] as List<dynamic>)
+          .map((e) => AllTeamModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      pagination:
+          Pagination.fromJson(json['pagination'] as Map<String, dynamic>),
+    );
 
 Map<String, dynamic> _$AllTeamsResponseModelToJson(
-  AllTeamsResponseModel instance,
-) => <String, dynamic>{
-  'data': instance.data,
-  'pagination': instance.pagination,
-};
+        AllTeamsResponseModel instance) =>
+    <String, dynamic>{
+      'data': instance.data,
+      'pagination': instance.pagination,
+    };
 
 Pagination _$PaginationFromJson(Map<String, dynamic> json) => Pagination(
-  count: (json['count'] as num).toInt(),
-  perPage: (json['per_page'] as num).toInt(),
-  currentPage: (json['current_page'] as num).toInt(),
-  nextPage: json['next_page'] as String?,
-  hasMore: json['has_more'] as bool,
-);
+      count: (json['count'] as num).toInt(),
+      perPage: (json['per_page'] as num).toInt(),
+      currentPage: (json['current_page'] as num).toInt(),
+      nextPage: json['next_page'] as String?,
+      hasMore: json['has_more'] as bool,
+    );
 
 Map<String, dynamic> _$PaginationToJson(Pagination instance) =>
     <String, dynamic>{
